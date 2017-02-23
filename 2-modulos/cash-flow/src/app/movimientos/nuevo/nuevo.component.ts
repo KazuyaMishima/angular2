900fb2a45062617ab25a3d44d8200ed1bec109b2
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-02-23 13:46:35 +0100

    2 clean

--- 2-modulos/cash-flow/src/app/movimientos/nuevo/nuevo.component.ts
+++ 2-modulos/cash-flow/src/app/movimientos/nuevo/nuevo.component.ts
@@ -9,26 +9,9 @@ import { Component, OnInit } from '@angular/core';
  *  Componente para crear movimientos
  **/
 export class NuevoComponent implements OnInit {
-  /** Propiedad con el array para montar la lista de tipos de movimientos posibles */
-  private tiposMovimiento: any[] = [
-    { id: 1, texto: 'Ingreso' },
-    { id: 2, texto: 'Gasto' }];
-  /** Propiedad en la que almacenar la información de un movimiento */
-  private movimiento: any = {};
 
   constructor() { }
-  /**
-   * Evento que se lanza al incio del ciclo de vida del componente
-   */
   ngOnInit() {
-    this.crearNuevoMovimiento();
-  }
 
-  private crearNuevoMovimiento() {
-    this.movimiento = {
-      importe: 0,
-      tipo: this.tiposMovimiento[0].id
-    };
   }
-
 }

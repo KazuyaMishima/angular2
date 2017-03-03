9319ed87b0f588adeb2a01f5866081881caabaed
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-03-03 13:23:09 +0100

    test init 5

--- 5-data-flow/cash-flow/src/app/movimientos/movimientos.module.ts
+++ 5-data-flow/cash-flow/src/app/movimientos/movimientos.module.ts
@@ -1,9 +1,9 @@
-import * as movimientos from './index';
+import * as movimientosIndex from './index';
 
 import { NgModule } from '@angular/core';
 
 /**
  * Una aplicación se compoen de unos o más módulos funcionales
  */
-@NgModule(movimientos.movimientosConfig)
+@NgModule(movimientosIndex.movimientosConfig)
 export class MovimientosModule { }

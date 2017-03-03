5084d30e54244750b4ced828de3c75f5018e2a39
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-03-03 10:44:04 +0100

    test observable

--- 4-injection/cash-flow/src/app/movimientos/datos.service.spec.ts
+++ 4-injection/cash-flow/src/app/movimientos/datos.service.spec.ts
@@ -1,16 +1,37 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+
+import { DatosService } from './datos.service';
+import {Movimiento} from './modelos/movimiento';
+import { Observable } from 'rxjs/Observable';
+
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { DatosService } from './datos.service';
+
+
+
 
 describe('DatosService', () => {
+  let datosService: DatosService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [DatosService]
     });
+    datosService = TestBed.get(DatosService);
   });
 
   it('should ...', inject([DatosService], (service: DatosService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should getNuevoMovimiento', () => {
+    const nuevoMovimiento = <Movimiento>datosService.getNuevoMovimiento();
+    expect(nuevoMovimiento).toBeTruthy();
+  });
+
+  it('should postMovimiento and Emit', () => {
+    datosService.getMovimientos$().subscribe((movimientos) => {
+      expect(movimientos.length > 0 ).toBeTruthy();
+    });
+    datosService.postMovimiento(datosService.getNuevoMovimiento());
+  });
 });

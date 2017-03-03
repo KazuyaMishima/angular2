9d771110371a843c6526be98e2cdeeed2eb70f81
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-03-03 14:19:20 +0100

    test movimiento nuevo

--- 5-data-flow/cash-flow/src/app/movimientos/nuevo/nuevo.component.spec.ts
+++ 5-data-flow/cash-flow/src/app/movimientos/nuevo/nuevo.component.spec.ts
@@ -5,19 +5,13 @@ import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { Movimiento } from './../modelos/movimiento';
+import { Testing } from './../../shared/testing/testing';
 
 /* tslint:disable:no-unused-variable */
-
-
-
-
-
-
-
-
 describe('NuevoComponent', () => {
   let component: movimientosIndex.NuevoComponent;
   let fixture: ComponentFixture<movimientosIndex.NuevoComponent>;
+  let testing: Testing;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule(movimientosIndex.movimientosConfig)
@@ -28,10 +22,21 @@ describe('NuevoComponent', () => {
     fixture = TestBed.createComponent(movimientosIndex.NuevoComponent);
     component = fixture.componentInstance;
     component.movimiento = new Movimiento(new Date(), 1, 0, 0);
+    testing = new Testing(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render a new movimiento', () => {
+    const importe: HTMLElement = testing.queryElementByCss('input[name="importe"]');
+    expect(component.movimiento.importe).toBe(1);
+    expect(testing.getModelValue(importe)).toContain('1');
+  });
+
+  it('should emit guardar event', () => {
     expect(component).toBeTruthy();
   });
 });
--- 5-data-flow/cash-flow/src/app/shared/testing/testing.ts
+++ 5-data-flow/cash-flow/src/app/shared/testing/testing.ts
@@ -1,4 +1,5 @@
 import { By } from '@angular/platform-browser';
+import { element } from 'protractor';
 
 export class Testing {
   constructor(private fixture) { }
@@ -9,5 +10,11 @@ export class Testing {
 
   queryAllByCss(css: string) {
     return this.fixture.debugElement.queryAll(By.css(css));
+  }
+
+  // importe.getAttribute('ng-reflect-model')
+
+  getModelValue(element: HTMLElement) {
+    return element.getAttribute('ng-reflect-model');
   }
 }

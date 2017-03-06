0a5956540d28a877f664ebf3136bda66dd3a59b5
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-03-06 09:59:23 +0100

    cli rc0 6 y 7

--- 7-http/cash-flow/src/main.ts
+++ 7-http/cash-flow/src/main.ts
@@ -1,14 +1,14 @@
-// importaciones de dependencias TypeScript al estilo ES6
-// primero los básicos para compatibilidad con navegadores
-import './polyfills.ts';
-
 // y por último la aplicación a leída desde el módulo raíz, llamado app por convenio.
+
 import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
-// después nuestro código, empezando por la configuración
 import { environment } from './environments/environment';
-// luego cosas de Angular
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// después nuestro código, empezando por la configuración
+
+// luego cosas de Angular
+
 
 // condiciones para ejecutar en modo desarrollo o producción
 if (environment.production) {

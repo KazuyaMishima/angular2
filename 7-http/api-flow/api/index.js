906806465a5a28b671a3741d71d04ef75eef77a1
Alberto Basalo <albertobasalo@hotmail.com>
2017-03-27 20:28:43 +0200

    api

--- 7-http/api-flow/api/index.js
+++ 7-http/api-flow/api/index.js
@@ -4,6 +4,9 @@ const usuarios = require('./usuarios.js');
 const sesiones = require('./sesiones.js');
 const maestros = require('./maestros.js');
 const movimientos = require('./movimientos.js');
+const mensajes = require('./mensajes.js');
+const eventos = require('./eventos.js');
+const items = require('./items.js');
 
 /** Función que configura las rutas de una aplicación */
 module.exports = app => {
@@ -11,4 +14,7 @@ module.exports = app => {
     sesiones(app, '/api/pub/sesiones');
     maestros(app, '/api/pub/maestros');
     movimientos(app, '/api/priv/movimientos');
+    mensajes(app, '/api/priv/mensajes');
+    eventos(app, '/api/priv/eventos');
+    items(app, '/api/priv/items');
 }

5685183951c6b3db9cad0b7466055f5f1e13eff6
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-03-27 10:58:56 +0200

    cash copy

---
+++ 8-forms/cash-flow/src/app/movimientos/nuevo/nuevo.component.ts
@@ -0,0 +1,89 @@
+import { FormUtils } from './../../shared/form-utils';
+import { positiveNumberValidator, betweenTwoDatesValidator } from '../../shared/custom-validators';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Categoria } from './../modelos/categoria';
+import { DatosService } from './../datos.service';
+import { Movimiento } from './../modelos/movimiento';
+import { Tipo } from './../modelos/tipo';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+
+@Component({
+  selector: 'cf-nuevo',
+  templateUrl: './nuevo.component.html',
+  styleUrls: ['./nuevo.component.css']
+})
+export class NuevoComponent implements OnInit {
+  private nuevoForm: FormGroup;
+  private formUtils: FormUtils;
+  tipos: Tipo[] = [];
+  categorias: Categoria[] = [];
+  movimiento: Movimiento;
+
+  constructor(private formBuilder: FormBuilder, private datosService: DatosService) { }
+
+  ngOnInit() {
+    this.movimiento = this.datosService.getNuevoMovimiento();
+    this.getTiposMovimiento();
+    this.buildForm();
+    this.formUtils = new FormUtils(this.nuevoForm);
+    this.onChangeTipo();
+  }
+
+  getTiposMovimiento() {
+    this.datosService.getTiposMovimiento$().subscribe(tipos => {
+      this.tipos = tipos;
+      this.datosService.getCategorias$().subscribe(categorias => {
+        this.categorias = this.datosService.getCategoriasPorTipo(this.movimiento.tipo);
+      });
+    });
+  }
+
+  buildForm() {
+    const currentDate = new Date(Date.now());
+    const pastDate = new Date(Date.now());
+    const futureDate = new Date(Date.now());
+
+    pastDate.setDate(currentDate.getDate() - 6);
+    futureDate.setDate(currentDate.getDate() + 5);
+
+    this.nuevoForm = this.formBuilder.group({
+      fecha: [this.movimiento.fecha.toISOString().substring(0, 10), [Validators.required, betweenTwoDatesValidator(pastDate, futureDate)]],
+      importe: [this.movimiento.importe, [Validators.required, positiveNumberValidator]],
+      tipo: 1,
+      categoria:  [[], [Validators.required]]
+    });
+  }
+
+  onChangeTipo() {
+    this.nuevoForm.get('tipo').valueChanges.subscribe((tipo) => {
+      this.datosService.getCategorias$().subscribe(categorias => {
+        this.categorias = this.datosService.getCategoriasPorTipo(tipo);
+        this.nuevoForm.patchValue({
+          categoria: []
+        });
+      });
+    });
+  }
+
+  alGuardarMovimiento({value, valid}): void {
+    this.datosService
+      .postMovimiento$(value)
+      .subscribe(r => console.log('Movimiento guardado'));
+  }
+
+  tieneErrores(field) {
+    return (this.hasChanges(field) && this.getfield(field).errors);
+  }
+
+  noEsValido(field) {
+    return (this.hasChanges(field) && !this.getfield(field).valid);
+  }
+
+  hasChanges(field) {
+    return (this.getfield(field).touched || this.getfield(field).dirty);
+  }
+
+  getfield(field) {
+    return this.nuevoForm.get(field);
+  }
+}
